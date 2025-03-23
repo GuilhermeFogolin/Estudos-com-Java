0f7a4c77e8e0b383355c8ca3f324d231a3bbb0d5
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-03-23 10:40:56 -0300

    Exercício IdadeDoEleitor

---
+++ JavaFECAP/src/EstudosGerais/IdadeDoEleitor.java
@@ -0,0 +1,26 @@
+package EstudosGerais;
+
+import java.util.Scanner;
+
+public class IdadeDoEleitor {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Informe a sua idade: ");
+		int idade = sc.nextInt();
+		
+		if(idade < 16) {
+			System.out.printf("Você tem %d anos e não pode voltar.", idade);
+		} else if (idade >= 16 && idade < 18 || idade >= 65) {
+			System.out.printf("Você tem %d anos e seu voto é facultativo.", idade);
+		} else if (idade >= 18 || idade < 65) {
+			System.out.printf("Você tem %d anos e seu voto é obrigatório.", idade);
+		} 
+		
+		sc.close();
+
+	}
+
+}
