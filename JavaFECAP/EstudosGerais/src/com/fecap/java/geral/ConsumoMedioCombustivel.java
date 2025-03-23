428c6a4d0ed951e5611e0ae800bb521bc99d04ae
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-03-23 10:26:33 -0300

    Exercício de calculadora média

---
+++ JavaFECAP/EstudosGerais/src/com/fecap/java/geral/ConsumoMedioCombustivel.java
@@ -0,0 +1,10 @@
+package com.fecap.java.geral;
+
+public class ConsumoMedioCombustivel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaFECAP/src/EstudosGerais/CalculoMedioCombustivel.java
@@ -0,0 +1,25 @@
+package EstudosGerais;
+
+import java.util.Scanner;
+
+public class CalculoMedioCombustivel {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos km foram percorridos? ");
+		double km = sc.nextDouble();
+		
+		System.out.print("Quantos litros de combustível? ");
+		double litros = sc.nextDouble();
+		
+		double resultado = km / litros;
+		
+		System.out.printf("\nO consumo médio de combustível é %.2f km/l.", resultado);
+		
+		sc.close();
+
+	}
+
+}
