9b909b627f6529b2578ab634bcb5716d2135d346
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-03-23 22:01:12 -0300

    Função sem retorno

---
+++ JavaFECAP/src/br/fecap/gerais/estudos/FuncaoImprimir.java
@@ -0,0 +1,14 @@
+package br.fecap.gerais.estudos;
+
+public class FuncaoImprimir {
+
+	public static void main(String[] args) {
+		
+		ola();
+	}
+	
+	public static void ola() {
+		System.out.println("Olá");
+	}
+
+}
