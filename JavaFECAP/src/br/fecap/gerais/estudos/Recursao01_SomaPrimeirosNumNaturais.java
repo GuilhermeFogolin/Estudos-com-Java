7ae02cb31188e26ba5636a8e62029eddffcf29da
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-04-01 20:01:04 -0300

    RecusÃao = SomaPrimeirosNumerosNaturais

---
+++ JavaFECAP/src/br/fecap/gerais/estudos/Recursao01_SomaPrimeirosNumNaturais.java
@@ -0,0 +1,28 @@
+package br.fecap.gerais.estudos;
+
+import java.util.Scanner;
+
+public class Recursao01_SomaPrimeirosNumNaturais {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o número: ");
+		int n = sc.nextInt();
+		
+		int resultado = SomaPrimeirosNumNaturais(n);
+		
+		System.out.print("A soma dos primeiros números de " + n + " vale: " + resultado + "!");
+		sc.close();
+	}
+	
+	public static int SomaPrimeirosNumNaturais(int num) {
+		
+		if(num <= 1) {
+			return num;
+		} else {
+			return SomaPrimeirosNumNaturais(num - 1) + num;
+		}		
+	}
+}
