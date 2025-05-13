09f8c9222c0f0c6c0ef2fb8e014ffe11d30c715e
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-05-13 19:31:07 -0300

    Implementando HashMap: put, get e remove

---
+++ JavaFECAP/src/br/fecap/gerais/estudos/HashMap01.java
@@ -0,0 +1,33 @@
+package br.fecap.gerais.estudos;
+
+import java.util.*;
+
+public class HashMap01 {
+
+    public static void main(String[] args) {
+
+        // Criando um novo HashMap: <String, Integer>. Chave String e valor Integer
+
+        Map<String, Integer> mapa = new HashMap<>();
+
+        // Adicionando valores
+
+        mapa.put("um", 1);
+        mapa.put("dois", 2);
+        mapa.put("três", 3);
+
+        // Imprimindo separadamente cada valor associado a sua chave respectiva
+
+        System.out.println(mapa.get("um"));
+        System.out.println(mapa.get("dois"));
+        System.out.println(mapa.get("três"));
+
+        // Remove a entrada associada à chave "três" do mapa
+
+        mapa.remove("três");
+
+        // Imprimindo o mapa após a remoção
+
+        System.out.println(mapa);
+    }
+}
