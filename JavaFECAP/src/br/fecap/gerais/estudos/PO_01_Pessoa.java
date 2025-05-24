f51c9a0dfe66ef8cee927432b460b2a28675d24d
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-05-24 16:14:05 -0300

    Sistema de cinema: Classe abstrata Pessoa

---
+++ JavaFECAP/src/br/fecap/gerais/estudos/PO_01_Pessoa.java
@@ -0,0 +1,29 @@
+package br.fecap.gerais.estudos;
+
+abstract class PO_01_Pessoa {
+
+    // Atributos
+
+    private String nome;
+
+    private String cpf;
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    // Construtor
+
+    public PO_01_Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    // Metodo
+
+    public abstract void print();
+}
