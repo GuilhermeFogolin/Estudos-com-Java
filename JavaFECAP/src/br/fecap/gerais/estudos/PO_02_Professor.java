2a824146d7e11993dbade9b9a20fcfbc5366ce9e
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-05-24 21:43:48 -0300

    Sistema de biblioteca: Professor

---
+++ JavaFECAP/src/br/fecap/gerais/estudos/PO_02_Professor.java
@@ -0,0 +1,30 @@
+package br.fecap.gerais.estudos;
+
+public class PO_02_Professor extends PO_02_Usuario {
+
+    private String departamento;
+    private String cargo;
+
+    public String getDepartamento() { return departamento; }
+
+    public void setDepartamento(String departamento) { this.departamento = departamento; }
+
+    public String getCargo() { return cargo; }
+
+    public void setCargo(String cargo) { this.cargo = cargo; }
+
+    // Construtor
+
+    public PO_02_Professor(String nome, String matricula, String departamento, String cargo) {
+        super(nome, matricula);
+        this.departamento = departamento;
+        this.cargo = cargo;
+    }
+
+    // Metodo
+
+    @Override
+    public boolean podeEmprestar() {
+        return getEmpAtivos().size() < 5;
+    }
+}
