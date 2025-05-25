11c58e9793eac239243899091942abb45405bb06
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-05-24 21:43:25 -0300

    Sistema de biblioteca: Emprestimo

---
+++ JavaFECAP/src/br/fecap/gerais/estudos/PO_02_Emprestimo.java
@@ -0,0 +1,41 @@
+package br.fecap.gerais.estudos;
+
+public class PO_02_Emprestimo {
+
+    private PO_02_Usuario usuario;
+    private PO_02_Livro livro;
+    private String dataRetirada;
+    private String dataDevolucao;
+
+    public PO_02_Usuario getUsuario() { return usuario; }
+
+    public void setUsuario(PO_02_Usuario usuario) { this.usuario = usuario; }
+
+    public PO_02_Livro getLivro() { return livro; }
+
+    public void setLivro(PO_02_Livro livro) { this.livro = livro; }
+
+    public String getDataRetirada() { return dataRetirada; }
+
+    public void setDataRetirada(String dataRetirada) { this.dataRetirada = dataRetirada; }
+
+    public String getDataDevolucao() { return dataDevolucao; }
+
+    public void setDataDevolucao(String dataDevolucao) { this.dataDevolucao = dataDevolucao; }
+
+    // Construtor
+
+    public PO_02_Emprestimo(PO_02_Usuario usuario,  PO_02_Livro livro, String dataRetirada, String dataDevolucao) {
+        this.usuario = usuario;
+        this.livro = livro;
+        this.dataRetirada = dataRetirada;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    // Metodo
+
+    // TODO: Adicionar usuário
+    public void registrarDevolucao() {
+        System.out.printf("O livro %s emprestado pelo usuário %s foi devolvido com sucesso!", livro.getTitulo(), usuario.getNome());
+    }
+}
