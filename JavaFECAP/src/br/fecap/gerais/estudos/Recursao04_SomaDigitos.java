4270f5147aad6030e5b70b971466e100db11d215
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-05-25 17:02:05 -0300

    Recursão: soma dos dígitos de um número

---
+++ JavaFECAP/src/br/fecap/gerais/estudos/Recursao04_SomaDigitos.java
@@ -0,0 +1,26 @@
+package br.fecap.gerais.estudos;
+
+import java.util.Scanner;
+
+public class Recursao04_SomaDigitos {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int n = sc.nextInt();
+
+        System.out.printf("\nA soma dos dígitos de %d vale %d", n, somaDigitos(n));
+
+    }
+
+    public static int somaDigitos(int n) {
+
+        if(n == 0) {
+            return 0;
+        } else {
+            return n % 10 +  somaDigitos(n / 10);
+        }
+    }
+}
