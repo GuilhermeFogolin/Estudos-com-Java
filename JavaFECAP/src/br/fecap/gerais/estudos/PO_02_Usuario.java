324e679ae2e3c717c18674f1bc534127463bff64
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-05-24 21:43:57 -0300

    Sistema de biblioteca: Usuario

---
+++ JavaFECAP/src/br/fecap/gerais/estudos/PO_02_Usuario.java
@@ -0,0 +1,49 @@
+package br.fecap.gerais.estudos;
+
+import java.util.ArrayList;
+
+public abstract class PO_02_Usuario {
+
+    private String nome;
+    private String matricula;
+    private ArrayList<PO_02_Emprestimo> empAtivos;
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getMatricula() { return matricula; }
+
+    public void setMatricula(String matricula) { this.matricula = matricula; }
+
+    public ArrayList<PO_02_Emprestimo> getEmpAtivos() { return empAtivos; }
+
+    public void setEmpAtivos(ArrayList<PO_02_Emprestimo> empAtivos) { this.empAtivos = empAtivos; }
+
+    // Construtor
+
+    public PO_02_Usuario(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.empAtivos = new ArrayList<>();
+    }
+
+    // Metodo
+
+    public abstract boolean podeEmprestar();
+
+    public void adicionaEmprestimo(PO_02_Emprestimo emp) {
+        if(podeEmprestar()) {
+            empAtivos.add(emp);
+        } else {
+            System.out.println("Limite de empréstimos atingidos!");
+        }
+    }
+
+    public void removeEmprestimo(PO_02_Emprestimo emp) {
+        empAtivos.remove(emp);
+    }
+
+
+
+}
