dd1b2363c3f80e61b1ac9d6bcb26299c0796d5ef
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-06-02 21:12:06 -0300

    Criando classe Consulta

---
+++ JavaFECAP/src/ClinicaVeterinaria/Consulta.java
@@ -0,0 +1,46 @@
+package ClinicaVeterinaria;
+
+import java.util.ArrayList;
+
+public class Consulta {
+
+    // Atributos
+
+    private String dataHora;
+    private String motivo;
+    private Animal animal;
+    private Veterinario vet;
+    private ArrayList<Procedimento> pro;
+
+    // Acessos
+
+    public String getDataHora() { return dataHora; }
+
+    public void setDataHora(String dataHora) { this.dataHora = dataHora; }
+
+    public String getMotivo() { return motivo; }
+
+    public void setMotivo(String motivo) { this.motivo = motivo; }
+
+    public Animal getAnimal() { return animal; }
+
+    public void setAnimal(Animal animal) { this.animal = animal; }
+
+    public Veterinario getVet() { return vet; }
+
+    public void setVet(Veterinario vet) { this.vet = vet; }
+
+    public ArrayList<Procedimento> getPro() { return pro; }
+
+    public void setPro(ArrayList<Procedimento> pro) { this.pro = pro; }
+
+    // Construtor
+
+    public Consulta(String dH, String m, Animal a, Veterinario v) {
+        this.dataHora = dH;
+        this.motivo   = m;
+        this.animal   = a;
+        this.vet      = v;
+        this.pro      = new ArrayList<>();
+    }
+}
