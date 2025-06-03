67d1996c2b483461d76cc0c5d947d6f4c5fccd85
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-06-02 21:25:34 -0300

    Refatorando impressão dos dados da consulta

--- JavaFECAP/src/ClinicaVeterinaria/Consulta.java
+++ JavaFECAP/src/ClinicaVeterinaria/Consulta.java
@@ -1,5 +1,7 @@
 package ClinicaVeterinaria;
 
+import br.fecap.gerais.estudos.Agregacao01_Professor;
+
 import java.util.ArrayList;
 
 public class Consulta {
@@ -42,5 +44,25 @@ public class Consulta {
         this.animal   = a;
         this.vet      = v;
         this.pro      = new ArrayList<>();
+    }
+
+    // Adicionando procedimentos
+
+    public void addProcedimento(Procedimento p) { pro.add(p); }
+
+    // Imprimindo informações
+
+    public void print() {
+        System.out.printf("\nInformações da consulta:" +
+                "\nAnimal: %s" +
+                "\nData de nascimento: %s" +
+                "\nMotivo: %s" +
+                "\nVeterinário responsável: %s" +
+                "\nData e hora: %s", animal.getNome(), animal.getDataNascimento(),
+                motivo, vet.getNome(), dataHora);
+        System.out.printf("\nProcedimentos realizados: ");
+        for(Procedimento p : pro) {
+            System.out.printf("\n- %s | R$%.2f", p.getNome(), p.getCusto());
+        }
     }
 }
