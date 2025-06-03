9f2d11218aded7d8f074b91e5ec04de4b1bfaaf1
Guilherme Fogolin <guilhermefogolin02@gmail.com>
2025-06-02 21:36:40 -0300

    Fazendo testes de classes e impressões

---
+++ JavaFECAP/src/ClinicaVeterinaria/Testes.java
@@ -0,0 +1,17 @@
+package ClinicaVeterinaria;
+
+public class Testes {
+
+    public static void main(String[] args) {
+
+        Cachorro cachorro   = new Cachorro("Apolo", "11/11/2022", "Cachorro", "Rottweilter");
+        Tutor tutor         = new Tutor("Fogolim", "(11)949251312", "123", "São Paulo", cachorro);
+        Veterinario vet     = new Veterinario("Carol", "(12)910235467", "321", "5456A-12", "Felinos");
+        Procedimento pro1   = new Procedimento("Rotina", "Consulta normal", 100);
+        Procedimento pro2   = new Procedimento("Raio-x", "Completo", 780.99);
+        Consulta con        = new Consulta("02-02-2025", "Doente", cachorro, vet);
+        con.addProcedimento(pro1);
+        con.addProcedimento(pro2);
+        con.print();
+    }
+}
